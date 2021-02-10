7c4dcf56ca3497971f0d9410ecbeecc528d53ff8
mariana-p25 <mariana.pardo.a@gmail.com>
2021-02-10 01:48:34 -0600

    acomodo correcto de lista hot drinks

--- lib/drinks/item_hot_drinks.dart
+++ lib/drinks/item_hot_drinks.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:estructura_practica_1/models/product_hot_drinks.dart';
 import 'package:estructura_practica_1/drinks/item_hot_drinks_details.dart';
@@ -20,40 +22,90 @@ class _ItemHotDrinksState extends State<ItemHotDrinks> {
     return Container(
       child: GestureDetector(
         onTap: _openDetails,
-        child: Card(
-          color: Color.fromARGB(0xFF, 0xBC, 0xB0, 0xA1),
-          child: (
-            ListTile(
-              leading: Image.network(drink.productImage,),
-              title: Text(
-                "${drink.productTitle}",
-                style: TextStyle(
-                  fontFamily: 'Akzidenz-Grotesk BQ Medium',
-                  fontSize: 15,
-                )
-              ),
-              subtitle: Column(
-                children: [
-                  Text(
-                    "${drink.productDescription}",
-                    style: TextStyle(
-                      fontFamily: 'Akzidenz-Grotesk BQ Medium',
-                      fontSize: 20,
-                      color: Colors.white
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                width: MediaQuery.of(context).size.width,
+                color: Color.fromARGB(0xFF, 0xBC, 0xB0, 0xA1),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 1.0, left: 16.0, top: 10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${drink.productTitle}",
+                            style: TextStyle(
+                            fontFamily: 'Akzidenz-Grotesk BQ Medium',
+                            fontSize: 20,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 24.0, bottom: 24.0,),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width/3,
+                              child: Text(
+                                "${drink.productDescription}",
+                                style: TextStyle(
+                                  fontFamily: 'Akzidenz-Grotesk BQ Medium',
+                                  fontSize: 20,
+                                  color: Colors.white
+                                )
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "\$${drink.productPrice}",
+                            style: TextStyle(
+                              fontFamily: 'Akzidenz-Grotesk BQ Medium',
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.network(
+                            drink.productImage, 
+                            width: 150,
+                            height: 150,
+                            fit: BoxFit.fill,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 1.0, top: 8.0),
+                      child: Column(
+                        children: [
+                          drink.liked?Icon(Icons.favorite_outline):Icon(Icons.favorite, color: Colors.black),
+                        ],
+                      ),
                     )
-                  ),
-                  Text(
-                    "\$${drink.productPrice}",
-                    style: TextStyle(
-                      fontFamily: 'Akzidenz-Grotesk BQ Medium',
-                      fontSize: 25,
-                    )
-                  ),
-                ],
+                  ],
+                ),
               ),
-              trailing: drink.liked?Icon(Icons.favorite_outline):Icon(Icons.favorite, color: Colors.black), 
-            )
-          ),
+            ),
+            Container(
+              
+            ),
+                    /*Text(
+                      "\$${drink.productPrice}",
+                      style: TextStyle(
+                        fontFamily: 'Akzidenz-Grotesk BQ Medium',
+                        fontSize: 25,
+                      )
+                    ),*/
+          ],
         ),
       ),
     );
